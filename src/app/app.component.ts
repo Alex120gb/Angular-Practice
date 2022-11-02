397ac301a5ccedb12cb727cf434183b5b49b0059
Alex120gb <alexis.krtz120@gmail.com>
2022-11-02 17:11:29 +0200

    First-Few-Tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-practice-app';
+  test = '';
+
+  sayhi(){
+    this.test = "hi";
+  };
+
+  resetParagraph() {
+    this.test = "";
+    this.title = "";
+  }
+
 }
